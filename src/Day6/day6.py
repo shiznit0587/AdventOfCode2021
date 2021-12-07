68ac5be78d3894119dfc3d65b66fcc92594c4999
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2021-12-06 23:40:07 -0500

    Completed Day 6 Part 2

--- src/Day6/day6.py
+++ src/Day6/day6.py
@@ -2,7 +2,9 @@ def day6():
     print('Running Day 6 - Part 1')
 
     with open('Day6/input.txt', 'r') as f:
-        fish = [int(s) for s in f.readline().strip().split(',')]
+        input = [int(s) for s in f.readline().strip().split(',')]
+
+    fish = input.copy()
 
     for _ in range(80):
         numfish = len(fish)
@@ -16,5 +18,19 @@ def day6():
     print(f'Total fish = {len(fish)}')
 
     print('Running Day 6 - Part 2')
+
+    fish = [0 for _ in range(7)]
+    nursery = [0, 0]
+    for f in input:
+        fish[f] += 1
+
+    for day in range(256):
+        matured = nursery[0]
+        nursery[0] = nursery[1]
+        spawn = day % 7
+        nursery[1] = fish[spawn]
+        fish[spawn] += matured
+
+    print(f'Total fish = {sum(fish) + sum(nursery)}')
 
     print("Day 6 Complete")
