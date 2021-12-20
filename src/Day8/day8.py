6c62b456cd622640216ed624734328380ee04126
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2021-12-20 12:37:27 -0500

    Completed Day 8 Part 1

--- src/Day8/day8.py
+++ src/Day8/day8.py
@@ -1,9 +1,28 @@
 def day8():
-    print("Running Day 8 - Part 1")
+    print('Running Day 8 - Part 1')
 
-    with open("Day8/input.txt", "r") as f:
-        lines = f.readlines()
+    with open('Day8/input.txt', 'r') as f:
+        entries = [Entry(l) for l in f.readlines()]
 
-    print("Running Day 8 - Part 2")
+    count = 0
+    for p in map(lambda e : e.output, entries):
+        for s in p.signals:
+            match len(s):
+                case (2|3|4|7):
+                    count += 1
 
-    print("Day 8 Complete")
+    print(f'Unique digit appearances = {count}')
+
+    print('Running Day 8 - Part 2')
+
+    print('Day 8 Complete')
+
+class Entry:
+    def __init__(self, line:str) -> None:
+        parts = [s.strip() for s in line.split('|')]
+        self.patterns = SignalPatterns(parts[0])
+        self.output = SignalPatterns(parts[1])
+
+class SignalPatterns:
+    def __init__(self, parts:str) -> None:
+        self.signals = parts.split()
