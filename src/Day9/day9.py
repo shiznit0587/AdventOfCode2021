8478e7667c7411fbe92294431e764dacc470e162
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2021-12-27 11:43:47 -0500

    Completed Day 9 Part 2

--- src/Day9/day9.py
+++ src/Day9/day9.py
@@ -1,3 +1,5 @@
+import math
+
 def day9():
     print('Running Day 9 - Part 1')
 
@@ -6,16 +8,32 @@ def day9():
 
     lowpoints = []
 
+    xmax, ymax = len(lines), len(lines[0])
+
     for i in range(len(lines)):
         for j in range(len(lines[i])):
             if all(map(
                 lambda p : int(lines[i][j]) < int(lines[p[0]][p[1]]), 
-                [p for p in getadjacent(i,j) if p[0] in range(len(lines)) and p[1] in range(len(lines[i]))])):
+                [p for p in getadjacent(i, j, xmax, ymax)])):
                 lowpoints.append((i,j))
-    
+
     print(f'Risk Level = {sum([int(lines[x][y]) + 1 for x,y in lowpoints])}')
 
     print('Running Day 9 - Part 2')
+
+    sizes = []
+
+    for lowpoint in lowpoints:
+        basin = set()
+        queue = [lowpoint]
+        while len(queue) != 0:
+            point = queue.pop()
+            if lines[point[0]][point[1]] != '9':
+                basin.add(point)
+                queue += [p for p in getadjacent(*point, xmax, ymax) if p not in basin]
+        sizes.append(len(basin))
+
+    print(f'Basin Size Product = {math.prod(sorted(sizes, reverse=True)[:3])}')
 
     print('Day 9 Complete')
 
@@ -23,8 +41,9 @@ def day9():
 Point = tuple[int, int]
 
 
-def getadjacent(x,y) -> list[Point]:
-    return [(x - 1, y),
+def getadjacent(x:int, y:int, xmax:int, ymax:int) -> list[Point]:
+    return [p for p in [(x - 1, y),
             (x + 1, y),
             (x, y - 1),
             (x, y + 1)]
+            if p[0] in range(xmax) and p[1] in range(ymax)]
