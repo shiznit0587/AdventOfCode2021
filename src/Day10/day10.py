de2c63dd23503706771c3f0d39139da67decabea
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2021-12-28 12:37:43 -0500

    Completed Day 10 Part 2

--- src/Day10/day10.py
+++ src/Day10/day10.py
@@ -4,7 +4,8 @@ def day10():
     with open('Day10/input.txt', 'r') as f:
         lines = [s.strip() for s in f.readlines()]
 
-    score = 0
+    c_score = 0
+    ac_scores = []
     for line in lines:
         ops, i, corrupted, = '', 0, False
         while not corrupted and i < len(line):
@@ -18,14 +19,23 @@ def day10():
                 case _:
                     corrupted = True
                     match c:
-                        case ')': score += 3
-                        case ']': score += 57
-                        case '}': score += 1197
-                        case '>': score += 25137
+                        case ')': c_score += 3
+                        case ']': c_score += 57
+                        case '}': c_score += 1197
+                        case '>': c_score += 25137
             i += 1
+        if not corrupted:
+            score = 0
+            for c in ops:
+                match c:
+                    case '(': score = score * 5 + 1
+                    case '[': score = score * 5 + 2
+                    case '{': score = score * 5 + 3
+                    case '<': score = score * 5 + 4
+            ac_scores.append(score)
+    ac_scores.sort()
 
-    print(f'Score = {score}')
-
+    print(f'Corruption Score = {c_score}')
     print('Running Day 10 - Part 2')
-
+    print(f'Autocomplete Score = {ac_scores[len(ac_scores)//2]}')
     print("Day 10 Complete")
