e995c15c7e82840edfa1eee5aef98d447f5caf01
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2022-01-02 01:17:01 -0500

    Completed Day 14 Part 2

--- src/Day14/day14.py
+++ src/Day14/day14.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 def day14():
     print('Running Day 14 - Part 1')
 
@@ -8,9 +9,9 @@ def day14():
         lines = f.readlines()
         template = lines[0].strip()
         for l in lines[2:]:
-            (a, b), insert = l.strip().split(' -> ')
-            rules[(a, b)] = insert
-            
+            (a, b), c = l.strip().split(' -> ')
+            rules[(a, b)] = c
+
     polymer = template
     for _ in range(10):
         prev = polymer
@@ -20,8 +21,39 @@ def day14():
         polymer += prev[-1:]
 
     counts = [polymer.count(c) for c in set(polymer)]
-    print(f'{max(counts) - min(counts)}')
+    print(f'Element Range Delta = {max(counts) - min(counts)}')
 
     print('Running Day 14 - Part 2')
+
+    char_counts = {c: template.count(c) for c in set(template)}
+    pair_counts = {}
+    for pair in itertools.pairwise(template):
+        if pair in pair_counts:
+            pair_counts[pair] += 1
+        else:
+            pair_counts[pair] = 1
+
+    for _ in range(40):
+        prev_pair_counts = pair_counts.copy()
+        for (a, b), c in rules.items():
+            if (a, b) in prev_pair_counts:
+                count = prev_pair_counts[a, b]
+                pair_counts[a, b] -= count
+                if c in char_counts:
+                    char_counts[c] += count
+                else:
+                    char_counts[c] = count
+                if (a, c) in pair_counts:
+                    pair_counts[a, c] += count
+                else:
+                    pair_counts[a, c] = count
+                if (c, b) in pair_counts:
+                    pair_counts[c, b] += count
+                else:
+                    pair_counts[c, b] = prev_pair_counts[a, b]
+
+    min_char = min(char_counts, key=char_counts.get)
+    max_char = max(char_counts, key=char_counts.get)
+    print(f'Element Range Delta = {char_counts[max_char] - char_counts[min_char]}')
 
     print("Day 14 Complete")
