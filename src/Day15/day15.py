87e23f901ea2e1715f9fe2ae8e37500685f059e7
shiznit0587 <17034738+shiznit0587@users.noreply.github.com>
2022-01-03 13:23:30 -0500

    Improved Day 15 performance of Dijkstra

--- src/Day15/day15.py
+++ src/Day15/day15.py
@@ -1,4 +1,3 @@
-import itertools
 import sys
 
 
@@ -20,10 +19,11 @@ def day15():
         # x, y = min({(x, y) for x, y in itertools.product(range(xmax), range(ymax)) if not visited[x][y]}, key=lambda p: risks[p[0]][p[1]])
 
         x, y = None, None
-        for i, j in itertools.product(range(xmax), range(ymax)):
-            if not visited[i][j]:
-                if x == None or risks[i][j] < risks[x][y]:
-                    x, y = i, j
+        for i in range(xmax):
+            for j in range(ymax):
+                if not visited[i][j]:
+                    if x == None or risks[i][j] < risks[x][y]:
+                        x, y = i, j
 
         for i, j in getadjacent(x, y, xmax, ymax):
             risk = risks[x][y] + ocean[i][j]
@@ -39,6 +39,16 @@ def day15():
     print("Day 15 Complete")
 
 
-def getadjacent(x: int, y: int, xmax: int, ymax: int) -> list[tuple[int, int]]:
-    return [p for p in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-            if p[0] in range(xmax) and p[1] in range(ymax)]
+def getadjacent(x: int, y: int, xmax: int, ymax: int):
+    # Changed to generator for performance
+    # return [p for p in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    # if p[0] in range(xmax) and p[1] in range(ymax)]
+
+    if 0 <= x - 1:
+        yield (x-1, y)
+    if x + 1 < xmax:
+        yield (x+1, y)
+    if 0 <= y - 1:
+        yield (x, y-1)
+    if y + 1 < ymax:
+        yield (x, y+1)
